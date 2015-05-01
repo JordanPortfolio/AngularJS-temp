44e64c7df6a47aaa65d6d10406b8784b49300987
Jordan Simpson <admin@jordanportfolio.com>
2015-05-01 07:53:08 -0700

    Add, edit, or remove the controller objects arrays.

--- app.js
+++ app.js
@@ -1,35 +1,49 @@
 angular.module('treehouseCourse', [])
-    .controller('stageTwoCtrl', function ($scope) {
-        $scope.user = {
-            name: "world",
-            bio: "<p>They were one man, not thirty. For as the one ship that held them all; though it was put together of all contrasting things&mdash;oak, and maple, and pine wood; iron, and pitch, and hemp&mdash;yet all these ran into each other in the one concrete hull, which shot on its way, both balanced and directed by the long central keel; even so, all the individualities of the crew, this man's valor, that man's fear; guilt and guiltiness, all varieties were welded into oneness, and were all directed to that fatal goal which Ahab their one lord and keel did point to.</p>"
-        };
-    })
-    .directive('hallo', function () {
-        return {
-            require: 'ngModel',
-            link: function ($scope, $element, $attrs, ngModelCtrl) {
-                $element.hallo({
-                    plugins: {
-                        'halloformat': {},
-                        'halloblock': {},
-                        'hallojustify': {},
-                        'hallolists': {},
-                        'halloreundo': {}
-                    }
-                });
-
-                ngModelCtrl.$render = function () {
-                    var contents = ngModelCtrl.$viewValue;
-                    $element.hallo('setContents', contents);
-                };
-
-                $element.on('hallomodified', function () {
-                    var contents = $element.hallo('getContents');
-                    ngModelCtrl.$setViewValue(contents);
-                    $scope.$digest();
-                });
-
+    .controller('stageCtrl', function ($scope, $http) {
+        var otherPeople = [
+            {
+                "name": "Jane",
+                "profession": "Designer",
+                "hometown": "New York, NY"
+            },
+            {
+                "name": "Jerry",
+                "profession": "Salesman",
+                "hometown": "Detroit, MI"
             }
+        ]
+
+        $http.get('people.json').success(function(people) {
+            $scope.people = people;
+        });
+
+        $scope.add = function () {
+            $scope.people.push(otherPeople.pop());
+        }
+
+        $scope.remove = function (person) {
+            otherPeople.push(person);
+            $scope.people = _.without($scope.people, person);
+        }
+
+        $scope.edit = function (person) {
+            $scope.editingPerson = person;
+        }
+
+        $scope.save = function (person) {
+            $http.post('people.json', person)
+                .success(function (response) {
+                    alert('Saved!');
+                })
+                .error(function (err) {
+                    alert('Error: '+err);
+                });
         }
     });
+
+
+
+
+
+
+
